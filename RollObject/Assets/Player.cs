23063d8eeded3225d7d39171ea607c58a47d4127
KawamuraMinato <kawamu.3710@gmail.com>
2019-04-05 01:31:01 +0900

    no message

---
+++ RollObject/Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    enum rollMode
+    {
+        not,
+        right,
+        reft,
+    }
+    float rollCount = 0;
+    const float ROLL_MAX = 90f;
+    const float ROLL_TIME = 1f;
+    const float ROLL_SPEED = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(rollCount);
+    }
+}
