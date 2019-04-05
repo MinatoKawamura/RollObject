fd7da6c8b818445dc5350d24107cd255fe5d13bd
KawamuraMinato <kawamu.3710@gmail.com>
2019-04-05 13:53:29 +0900

    no message

---
+++ RollObject/Assets/CoreMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreMove : MonoBehaviour
+{
+    float pos1 = 0.5f;
+    float pos2 = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.localPosition = new Vector3(pos1, 0f, pos2);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.localPosition = new Vector3(pos1, 0f, pos1);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.localPosition = new Vector3(pos2, 0f, pos1);
+        }
+    }
+}
--- RollObject/Assets/Player.cs
+++ RollObject/Assets/Player.cs
@@ -4,25 +4,66 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    enum rollMode
+    enum ROLL_MODE
     {
-        not,
-        right,
-        reft,
+        not,    //回転していない
+        right,  //右回転
+        reft,   //左回転
     }
+    ROLL_MODE mode = ROLL_MODE.not;
+    Vector3 pos;
     float rollCount = 0;
     const float ROLL_MAX = 90f;
-    const float ROLL_TIME = 1f;
-    const float ROLL_SPEED = 30f;
+    const float ROLL_TIME = 2f;
+    Transform corePos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (Transform core in gameObject.transform)
+        {
+            if (core.name == "Core")
+            {
+                corePos = core.transform;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(rollCount);
+        //inputKey();
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //transform.Rotate(new Vector3(0f, rollSpeed() * Time.deltaTime));
+            transform.RotateAround(corePos.position, transform.up, ROLL_MAX);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //transform.Rotate(new Vector3(0f, -rollSpeed() * Time.deltaTime));
+            transform.RotateAround(corePos.position, transform.up, -ROLL_MAX);
+        }
+    }
+
+    //void roll()
+    //{
+
+    //}
+
+    //void inputKey()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.A))
+    //    {
+    //        mode = ROLL_MODE.reft;
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.D))
+    //    {
+    //        mode = ROLL_MODE.right;
+    //    }
+    //}
+
+    float rollSpeed()
+    {
+        return ROLL_MAX / ROLL_TIME;
     }
 }
