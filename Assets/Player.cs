78a56edf3be741e6a4366e37ae374638773c98a6
KawamuraMinato <kawamu.3710@gmail.com>
2019-06-02 23:26:41 +0900

    no message

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -10,15 +10,20 @@ public class Player : MonoBehaviour
         right = 1,  //右回転
         left = -1,  //左回転
     }
-    //IEnumerator roll;
     ROLL_MODE mode = ROLL_MODE.not;
     const float ROLL_MAX = 90f;
     const float ROLL_TIME = 2f;
     Transform corePos;
     float rollCount = 0f;
+
+    //test
+    Rigidbody rBody;
+    Vector3 speed;
+
     // Start is called before the first frame update
     void Start()
     {
+        //回転の中心のオブジェクトを取得する
         foreach (Transform core in gameObject.transform)
         {
             if (core.name == "Core")
@@ -26,40 +31,33 @@ public class Player : MonoBehaviour
                 corePos = core.transform;
             }
         }
+
+        //test
+        rBody = GetComponent<Rigidbody>();
+        speed = new Vector3(0, 90, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputKey();
-        roll();
+        //inputKey();
+        //roll();
     }
 
+    //キー入力したら左右どちらの回転かを判断
     void inputKey()
     {
         if (Input.GetKeyDown(KeyCode.A) && mode == ROLL_MODE.not)
         {
             mode = ROLL_MODE.right;
-            //roll = Roll();
-            //StartCoroutine(roll);
         }
         if (Input.GetKeyDown(KeyCode.D) && mode == ROLL_MODE.not)
         {
             mode = ROLL_MODE.left;
-            //roll = Roll();
-            //StartCoroutine(roll);
         }
     }
 
-    //IEnumerator Roll()
-    //{
-    //    while (true)
-    //    {
-    //        transform.RotateAround(corePos.position, transform.up, rollSpeed() * (int)mode);
-    //        yield return new WaitForSeconds(ROLL_TIME);
-    //    }
-    //}
-
+    //回転させる
     void roll()
     {
         if (rollCount < ROLL_MAX)
@@ -77,5 +75,11 @@ public class Player : MonoBehaviour
     float rollSpeed()
     {
         return ROLL_MAX / ROLL_TIME;
+    }
+
+    void TestRoll()
+    {
+        Quaternion deltaRotation = Quaternion.Euler(speed * Time.deltaTime);
+        rBody.MoveRotation(rBody.rotation * deltaRotation);
     }
 }
