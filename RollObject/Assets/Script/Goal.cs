592f65077c992f91cc11bfcef9b0bfcb6984b67d
KawamuraMinato <kawamu.3710@gmail.com>
2019-07-22 15:34:21 +0900

    ゴールとタイトルのUI追加

--- RollObject/Assets/Script/Goal.cs
+++ RollObject/Assets/Script/Goal.cs
@@ -1,21 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
     Player player;
+    GameObject canvas;
+    public bool goalFlag = false;
+    float LoadTime = 0f;
+    const float LOAD_TIME_MAX = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
+        canvas = GameObject.Find("Canvas");
         player = GameObject.Find("Player").GetComponent<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GoalLoadScene();
+    }
+
+    void GoalLoadScene()
+    {
+        if (goalFlag)
+        {
+            LoadTime += Time.deltaTime;
+            if(LoadTime >= 5f)
+            {
+                goalFlag = false;
+                SceneManager.LoadScene("Title");
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -25,7 +44,8 @@ public class Goal : MonoBehaviour
             player.goalCount++;
             if (player.goalCount >= player.box.Count)
             {
-                Debug.Log("クリア");
+                goalFlag = true;
+                canvas.transform.GetChild(0).gameObject.SetActive(true);
             }
         }
     }
